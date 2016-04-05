bec7d91c80ab79aa25ceb49f61728ae15de44dd3
Ethan <erdesi90562@gmail.com>
2016-04-05 15:26:26 -0400

    Update user signin to use Bookshelf

--- express/app/controllers/users.js
+++ express/app/controllers/users.js
@@ -41,65 +41,69 @@ const makeErrorHandler = (res, next) =>
 
 const signup = (req, res, next) => {
   let credentials = req.body.credentials;
-  let user = { email: credentials.email };
+  let user = {
+                email: credentials.email,
+                password: credentials.password,
+              };
   getToken().then(token =>
     user.token = token
-  ).then(() =>
-    new User(user).setPassword(credentials.password)
-  ).then(newUser => {
-    let user = newUser.toObject();
-    delete user.token;
+  )
+  .then(() => {
+    return new User(user).save();
+  })
+  .then(newUser => {
+    let user = newUser.attributes;
     delete user.passwordDigest;
     res.json({ user });
-  }).catch(makeErrorHandler(res, next));
-
+  })
+  .catch(makeErrorHandler(res, next));
 };
 
 const signin = (req, res, next) => {
-  let credentials = req.body.credentials;
-  let search = { email: credentials.email };
-  User.findOne(search
-  ).then(user =>
-    user ? user.comparePassword(credentials.password) :
-          Promise.reject(new HttpError(404))
-  ).then(user =>
-    getToken().then(token => {
-      user.token = token;
-      return user.save();
-    })
-  ).then(user => {
-    user = user.toObject();
-    delete user.passwordDigest;
-    res.json({ user });
-  }).catch(makeErrorHandler(res, next));
+  // let credentials = req.body.credentials;
+  // let search = { email: credentials.email };
+  // User.findOne(search
+  // ).then(user =>
+  //   user ? user.comparePassword(credentials.password) :
+  //         Promise.reject(new HttpError(404))
+  // ).then(user =>
+  //   getToken().then(token => {
+  //     user.token = token;
+  //     return user.save();
+  //   })
+  // ).then(user => {
+  //   user = user.toObject();
+  //   delete user.passwordDigest;
+  //   res.json({ user });
+  // }).catch(makeErrorHandler(res, next));
 };
 
 const signout = (req, res, next) => {
-  getToken().then(token =>
-    User.findOneAndUpdate({
-      _id: req.params.id,
-      token: req.currentUser.token,
-    }, {
-      token,
-    })
-  ).then((user) =>
-    user ? res.sendStatus(200) : next()
-  ).catch(next);
+  // getToken().then(token =>
+  //   User.findOneAndUpdate({
+  //     _id: req.params.id,
+  //     token: req.currentUser.token,
+  //   }, {
+  //     token,
+  //   })
+  // ).then((user) =>
+  //   user ? res.sendStatus(200) : next()
+  // ).catch(next);
 };
 
 const changepw = (req, res, next) => {
   debug('Changing password');
-  User.findOne({
-    _id: req.params.id,
-    token: req.currentUser.token,
-  }).then(user =>
-    user ? user.comparePassword(req.body.passwords.old) :
-      Promise.reject(new HttpError(404))
-  ).then(user =>
-    user.setPassword(req.body.passwords.old)
-  ).then((/* user */) =>
-    res.sendStatus(200)
-  ).catch(makeErrorHandler(res, next));
+  // User.findOne({
+  //   _id: req.params.id,
+  //   token: req.currentUser.token,
+  // }).then(user =>
+  //   user ? user.comparePassword(req.body.passwords.old) :
+  //     Promise.reject(new HttpError(404))
+  // ).then(user =>
+  //   user.setPassword(req.body.passwords.old)
+  // ).then((/* user */) =>
+  //   res.sendStatus(200)
+  // ).catch(makeErrorHandler(res, next));
 };
 
 module.exports = controller({
--- express/app/models/user.js
+++ express/app/models/user.js
@@ -15,9 +15,15 @@ const bookshelf = require("bookshelf")(Knex);
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('users', function(table) {
     table.increments('id').notNullable().primary();
-    table.string('email'); // .notNullable().unique();
+    table.string('email').notNullable().unique();
     table.string('token').unique();
     table.string('passwordDigest').notNullable();
+    table.timestamps();
+  }).createTableIfNotExists('temperatures', function(table) {
+    table.increments('id').notNullable().primary();
+    table.string('main_topic');
+    table.string('data_topic');
+    table.float('data');
     table.timestamps();
   });
 };
@@ -25,16 +31,16 @@ exports.up = function(knex, Promise) {
 exports.up(Knex)
 .then(() => {
   console.log('Table created sucessfully');
-  //Knex.destroy();
-  //console.log("Connection closed..");
-});
+})
+.catch((err) => { console.log('opps little error keep going.');});
 
 let User = bookshelf.Model.extend({
   tableName: 'users',
   initialize: function() {
-    this.on('creating', this.hashPassword, this);
+    this.on('creating', this.setPassword, this);
+    this.on('updating', this.comparePassword, this);
   },
-  hashPassword: function (password) {
+  setPassword: function (password) {
     var _this = this;
     return new Promise((resolve, reject) =>
       bcrypt.genSalt(null, (err, salt) =>
@@ -48,42 +54,60 @@ let User = bookshelf.Model.extend({
     .then((digest) => {
       _this.attributes.passwordDigest = digest;
       delete _this.attributes.password;
-      console.log(_this);
       return _this;
+    });
+  },
+  comparePassword: function (password) {
+    let _this = this;
+    new User({ 'email': _this.attributes.email })
+    .fetch()
+    .then((data) => {
+      let passwordDigest = data.attributes.passwordDigest;
+      let dbUser = data;
+      // console.log('Compare Password: passwordDigest: ', passwordDigest);
+      // console.log('Compare Password: password: ', _this.attributes.password);
+      return new Promise((resolve, reject) =>
+        bcrypt.compare(_this.attributes.password, passwordDigest, (err, data) =>
+          err ? reject(err) : data ? resolve(data) : reject(new Error('Not Authorized')))
+        )
+        .then((data) => {
+          // trying to send back user in data base.
+          if (data) {
+            console.log('\n New Promise dbUser: ', dbUser.attributes);
+            return dbUser.attributes;
+          }
+        });
     });
   },
 });
 
-//new User({ email: 'hank@statefarm.com', password: 'hank'}).save();
-let found = User.query('where', 'passwordDigest', '=', '$2a$10$sddavB.jzpRyL9Kj0HIPSxiU99oKFwuyKqgSO.').fetch().then(console.log);
+// let user = {
+//   email: 'jake@farm.com',
+// //  password: 'password',
+// //  token: 'Mq6jGEk4bgpzICalHLwm3Q=='
+// };
+//
+// let patchUser = {
+//   email: 'jake@farm.com',
+// };
+
+// new User({ email: 'kake@statefarm.com', password: 'password'}).save();
+// new User({ email: 'OnthisShit', password: 'password'}).save();
+// new User({ email: 'jake@farm.com', password: 'password'}).save();
+// new User({ email: 'jess@statefarm.com', password: 'password'}).save();
+// let found = User.query('where', 'passwordDigest', '=', '$2a$10$sddavB.jzpRyL9Kj0HIPSxiU99oKFwuyKqgSO.').fetch().then(console.log);
 //console.log(found);
 
-User.methods.comparePassword = function (password) {
-  let _this = this;
+// new User(user).fetch().then((data) => {
+//   //console.log('BEFORE: this is the fetch data:  ', data,'\n');
+// }).then(() => {
+//   new User(patchUser).save({ password: 'password' }, { patch: true }).then(console.log).then(() => {
+//     new User(user).fetch().then((data) => {
+//       console.log('AFTER: this is the fetch data: ', data, '\n');
+//     });
+//   });
+// });
 
-  return new Promise((resolve, reject) =>
-    bcrypt.compare(password, _this.passwordDigest, (err, data) =>
-        err ? reject(err) : data ? resolve(data) : reject(new Error('Not Authorized')))
-    ).then(() => _this);
-};
-
-User.methods.setPassword = function (password) {
-  var _this = this;
-
-  return new Promise((resolve, reject) =>
-    bcrypt.genSalt(null, (err, salt) =>
-        err ? reject(err) : resolve(salt))
-  ).then((salt) =>
-    new Promise((resolve, reject) =>
-      bcrypt.hash(password, salt, (err, data) =>
-        err ? reject(err) : resolve(data)))
-  ).then((digest) => {
-    _this.passwordDigest = digest;
-    return _this.save();
-  });
-};
-
-// const User = exports.up()
-const User = User.model('User', User);
+// new User(user).save([key], [val], [attrs], [options]);
 
 module.exports = User;
