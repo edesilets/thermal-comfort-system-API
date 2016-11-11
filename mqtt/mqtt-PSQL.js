79315ed45bafa07bc3f5f03ecb6c22c2711e7e12
Ethan <erdesi90562@gmail.com>
2016-11-11 14:55:41 -0500

    Store values only if different from old value

--- mqtt/mqtt-PSQL.js
+++ mqtt/mqtt-PSQL.js
@@ -22,6 +22,8 @@ let client  = mqtt.connect('mqtt://homestatus.ddns.net', {
   protocolVersion: 3
 });
 
+let topics = [];
+
 client.on('connect', function () {
   client.subscribe('#');
   console.log('subscribed and now published');
@@ -29,7 +31,11 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   // /home/livingroom/thermostat/temperature/livingroom
-  let topicArray = topic.toString().split('/');
+  let messageString = message.toString().replace(' ', '');
+  let floatMessage = parseFloat(messageString);
+
+  let topicStr = topic.toString();
+  let topicArray = topicStr.split('/');
   let db = topicArray[1];
   let mainLocation = topicArray[2];
   let item = topicArray[3];
@@ -37,22 +43,26 @@ client.on('message', function (topic, message) {
   let sensorLocation = topicArray[5];
 
   if (topicArray.length === 6) {
-    let messageString = message.toString().replace(' ', '');
-    let floatMessage = parseFloat(messageString);
-    console.log(`Message: ${messageString}, Topic: ${topic.toString()}`);
+    if (topics[topicStr] === undefined) {
+      topics[topicStr] = messageString;
+    } else {
+      // console.log(`Message: ${messageString}, Topic: ${topic.toString()}`);
+      if (!(topics[topicStr] === messageString)) {
+        // console.log(`Save temps TemperatureNew: ${messageString}, TemperatureOld: ${topics[topicStr]}`);
+        let data = {
+          item: item,
+          main_location: mainLocation,
+          sensor_type: sensorType,
+          sensor_location: sensorLocation,
+          data: floatMessage,
+          created_at: moment().format(),
+          updated_at: moment().format()
+        };
 
-    let data = {
-      item: item,
-      main_location: mainLocation,
-      sensor_type: sensorType,
-      sensor_location: sensorLocation,
-      data: floatMessage,
-      created_at: moment().format(),
-      updated_at: moment().format()
+        topics[topicStr] = messageString;
+        new status(data).save();
+      }
     }
-    console.log(data);
-
-    new status(data).save();
   }
 });
 
